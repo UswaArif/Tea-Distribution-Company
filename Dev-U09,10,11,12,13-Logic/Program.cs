bfd9c00e8eb8e46bf2282ae00b8288da63d94d89
MutaibaMohsin <mutaibamohsin845@gmail.com>
2022-12-14 00:44:57 +0500

    Dev-Logics

---
+++ Dev-U09,10,11,12,13-Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dev_U09_10_11_12_13_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Dev-U35-Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dev_U35_Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
