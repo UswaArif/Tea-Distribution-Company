2d42bbbe94832ab04daced28febb181c36da6f14
UswaArif <uswaarif32@gmail.com>
2022-12-14 16:01:05 +0500

    Dev-U04,U05,U06-UI

---
+++ FinalProject/FinalProject/DL/RiderDL.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.BL;
+using System.IO;
+using SignIn.BL;
+
+namespace Project.DL
+{
+    class RiderDL
+    {
+        private static LinkedList<RiderBL> riderList = new LinkedList<RiderBL>();
+
+        public static LinkedList<RiderBL> RiderList { get => riderList; set => riderList = value; }
+
+        public static void addRiderIntoList(RiderBL r1)
+        {
+            RiderList.AddLast(r1);
+        }
+
+        public static RiderBL isRider(string Id)
+        {
+            foreach (RiderBL r1 in RiderList)
+            {
+                if (r1.EmpId1 == Id)
+                {
+                    return r1;
+                }
+            }
+            return null;
+        }
+
+        public static RiderBL searchRider(string ID)
+        {
+            foreach (RiderBL r1 in RiderList)
+            {
+                if (r1.EmpId1 == ID)
+                {
+                    return r1;
+                }
+            }
+            return null;
+        }
+
+        public static void editRider(RiderBL previous,RiderBL updated)
+        {
+            foreach (RiderBL r1 in RiderList)
+            {
+                if (r1.EmpName1 == previous.EmpName1 && r1.EmpCnic1 == previous.EmpCnic1 && r1.EmpPhone1 == previous.EmpPhone1 && r1.EmpId1 == previous.EmpId1 && r1.EmpSalary1 == previous.EmpSalary1 && r1.VehicleId1 == previous.VehicleId1)
+                {
+                    r1.EmpName1 = updated.EmpName1;
+                    r1.EmpCnic1 = updated.EmpCnic1;
+                    r1.EmpPhone1 = updated.EmpPhone1;
+                    r1.EmpId1 = updated.EmpId1;
+                    r1.EmpSalary1 = updated.EmpSalary1;
+                    r1.VehicleId1 = updated.VehicleId1;
+                }
+            }
+        }
+
+        public static void checkIsDeleted(RiderBL delete)
+        {
+            if (delete != null)
+            {
+                foreach (RiderBL r1 in RiderList)
+                {
+                    if (r1.EmpName1 == delete.EmpName1 && r1.EmpCnic1 == delete.EmpCnic1 && r1.EmpPhone1 == delete.EmpPhone1 && r1.EmpId1 == delete.EmpId1 && r1.EmpSalary1 == delete.EmpSalary1 && r1.VehicleId1 == delete.VehicleId1)
+                    {
+                        RiderList.Remove(r1);
+                    }
+                }
+            }
+        }
+
+        public static bool readfromFile(string path)
+        {
+            StreamReader f = new StreamReader(path);
+            string record;
+            if (File.Exists(path))
+            {
+                while ((record = f.ReadLine()) != null)
+                {
+                    string[] splittedRecord = record.Split(',');
+                    string name = splittedRecord[0];
+                    string Id = splittedRecord[1];
+                    string phoneNumber = splittedRecord[2];
+                    int salary = int.Parse(splittedRecord[3]);
+                    string CNIC = splittedRecord[4];
+                    bool isSalaryPaid = bool.Parse(splittedRecord[5]);
+                    float latitude = float.Parse(splittedRecord[6]);
+                    float longitude = float.Parse(splittedRecord[7]);
+                    string vehicleAssign = splittedRecord[8];
+                    int totalDelivery = int.Parse(splittedRecord[9]);
+                    
+                    area a = new area(latitude,longitude);
+
+                    RiderBL r = new RiderBL(name,CNIC,phoneNumber,Id,salary,vehicleAssign,totalDelivery,isSalaryPaid,a);
+                    addRiderIntoList(r);
+                }
+                f.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void storeIntoFile(string path, RiderBL r)
+        {
+            StreamWriter f = new StreamWriter(path, true);
+            f.WriteLine(r.EmpName1 + "," + r.EmpId1 + "," + r.EmpPhone1 + "," + r.EmpSalary1 + "," + r.EmpCnic1 + "," + r.IsSalaryPaid + "," + r.RiderArea1.Latitude1 + "," + r.RiderArea1.Longitude1 + "," + r.TotalDelivery1);
+            f.Flush();
+            f.Close();
+        }
+
+        public static void storeAllRiderIntoFile(string path)
+        {
+            StreamWriter f = new StreamWriter(path);
+            foreach (RiderBL storedrider in RiderList)
+            { 
+                f.WriteLine(storedrider.EmpName1 + "," + storedrider.EmpId1 + "," + storedrider.EmpPhone1 + "," + storedrider.EmpSalary1 + "," + storedrider.EmpCnic1 + "," + storedrider.IsSalaryPaid + "," + storedrider.RiderArea1.Latitude1 + "," + storedrider.RiderArea1.Longitude1 + "," + storedrider.TotalDelivery1);
+
+            }
+            f.Flush();
+
+            f.Close();
+        }
+    }
+}
---
+++ FinalProject/FinalProject/EditRider.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace FinalProject
+{
+    partial class EditRider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RiderSalarytxt = new System.Windows.Forms.TextBox();
+            this.RiderPhonetxt = new System.Windows.Forms.TextBox();
+            this.RiderCinictxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RiderNametxt = new System.Windows.Forms.TextBox();
+            this.RiderIDtxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Backbtn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.RiderVechiletxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RiderSalarytxt
+            // 
+            this.RiderSalarytxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderSalarytxt.Location = new System.Drawing.Point(190, 164);
+            this.RiderSalarytxt.Name = "RiderSalarytxt";
+            this.RiderSalarytxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderSalarytxt.TabIndex = 21;
+            // 
+            // RiderPhonetxt
+            // 
+            this.RiderPhonetxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderPhonetxt.Location = new System.Drawing.Point(190, 124);
+            this.RiderPhonetxt.Name = "RiderPhonetxt";
+            this.RiderPhonetxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderPhonetxt.TabIndex = 20;
+            // 
+            // RiderCinictxt
+            // 
+            this.RiderCinictxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderCinictxt.Location = new System.Drawing.Point(190, 84);
+            this.RiderCinictxt.Name = "RiderCinictxt";
+            this.RiderCinictxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderCinictxt.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter Rider\'s Name";
+            // 
+            // RiderNametxt
+            // 
+            this.RiderNametxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderNametxt.Location = new System.Drawing.Point(190, 7);
+            this.RiderNametxt.Name = "RiderNametxt";
+            this.RiderNametxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderNametxt.TabIndex = 2;
+            // 
+            // RiderIDtxt
+            // 
+            this.RiderIDtxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderIDtxt.Location = new System.Drawing.Point(190, 44);
+            this.RiderIDtxt.Name = "RiderIDtxt";
+            this.RiderIDtxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderIDtxt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Enter ID";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Enter Salary";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(106, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Enter Phone Number";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(627, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 166);
+            this.pictureBox2.TabIndex = 29;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(326, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edit Rider";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Red;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 288F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 408F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(796, 48);
+            this.tableLayoutPanel1.TabIndex = 26;
+            // 
+            // Backbtn
+            // 
+            this.Backbtn.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Backbtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Backbtn.FlatAppearance.BorderSize = 2;
+            this.Backbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Backbtn.Location = new System.Drawing.Point(17, 11);
+            this.Backbtn.Name = "Backbtn";
+            this.Backbtn.Size = new System.Drawing.Size(75, 29);
+            this.Backbtn.TabIndex = 2;
+            this.Backbtn.Text = "Back";
+            this.Backbtn.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Enter CNIC";
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 2;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+            this.tableLayoutPanel5.Controls.Add(this.Addbtn, 1, 0);
+            this.tableLayoutPanel5.Controls.Add(this.Backbtn, 0, 0);
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(610, 398);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 1;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(188, 51);
+            this.tableLayoutPanel5.TabIndex = 28;
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.Addbtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Addbtn.FlatAppearance.BorderSize = 2;
+            this.Addbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Addbtn.Location = new System.Drawing.Point(110, 11);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(75, 29);
+            this.Addbtn.TabIndex = 1;
+            this.Addbtn.Text = "Edit";
+            this.Addbtn.UseVisualStyleBackColor = false;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // RiderVechiletxt
+            // 
+            this.RiderVechiletxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RiderVechiletxt.Location = new System.Drawing.Point(190, 206);
+            this.RiderVechiletxt.Name = "RiderVechiletxt";
+            this.RiderVechiletxt.Size = new System.Drawing.Size(316, 20);
+            this.RiderVechiletxt.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Enter Vehicle  Assigned";
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.42238F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.57761F));
+            this.tableLayoutPanel4.Controls.Add(this.RiderSalarytxt, 1, 4);
+            this.tableLayoutPanel4.Controls.Add(this.RiderPhonetxt, 1, 3);
+            this.tableLayoutPanel4.Controls.Add(this.RiderCinictxt, 1, 2);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.RiderNametxt, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.RiderIDtxt, 1, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel4.Controls.Add(this.label8, 0, 3);
+            this.tableLayoutPanel4.Controls.Add(this.label6, 0, 2);
+            this.tableLayoutPanel4.Controls.Add(this.RiderVechiletxt, 1, 5);
+            this.tableLayoutPanel4.Controls.Add(this.label4, 0, 5);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(40, 95);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 6;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.22222F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.77778F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(539, 238);
+            this.tableLayoutPanel4.TabIndex = 27;
+            // 
+            // EditRider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel5);
+            this.Controls.Add(this.tableLayoutPanel4);
+            this.Name = "EditRider";
+            this.Text = "EditRider";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox RiderSalarytxt;
+        private System.Windows.Forms.TextBox RiderPhonetxt;
+        private System.Windows.Forms.TextBox RiderCinictxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox RiderNametxt;
+        private System.Windows.Forms.TextBox RiderIDtxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Backbtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.TextBox RiderVechiletxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+    }
+}
