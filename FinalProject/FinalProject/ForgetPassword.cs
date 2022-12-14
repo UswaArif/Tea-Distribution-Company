dd12753916671bc58598787306110f8326be60fe
mahnoorhassan123 <mahnoorh344@gmail.com>
2022-12-14 15:02:32 +0500

    Dev-U01,U02,U03-UI

---
+++ FinalProject/FinalProject/ForgetPassword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SignIn.BL;
+
+namespace FinalProject
+{
+    public partial class ForgetPassword : Form
+    {
+        UserBL newUser;
+        public ForgetPassword(UserBL u)
+        {
+            InitializeComponent();
+            newUser = u;
+        }
+
+        private void resetbtn_Click(object sender, EventArgs e)
+        {
+            if (ForgotPasswordtxt.Text == ReEntertxt.Text)
+            {
+                newUser.UserPassword1 = ForgotPasswordtxt.Text;
+            }
+            else
+            {
+                MessageBox.Show("InCorrect Password");
+            }
+        }
+
+        private void backbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ FinalProject/FinalProject/SignIn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SignIn.BL;
+using SignIn.DL;
+
+namespace FinalProject
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            UserBL newUser = new UserBL(UserNametxt.Text, UserPasswordtxt.Text);
+            UserBL Checkresult = UserDL.checkUser(newUser);
+            if (Checkresult != null)
+            {
+                //if(Checkresult.isAdmin());
+            }
+            else
+            {
+
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Form moreForm = new ForgetPassword();
+            //moreForm.Show();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form moreForm = new SignUp();
+            moreForm.Show();
+        }
+    }
+}
